0b5d523bf90f32a5f896b75584e4c1b20246bda3
Jayd Schumacher <jaydschumacher@gmail.com>
2018-05-03 10:10:37 -0700

    Setup webpack

--- src/app.js
+++ src/app.js
@@ -3,3 +3,31 @@ import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
       
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+
+
+// class OldSyntax {
+//     constructor() {
+//         this.name = 'John';
+//         this.getGreeting = this.getGreeting.bind(this);
+//     }
+//     getGreeting() {
+//         return `Hi. My name is ${this.name}.`;
+//     }
+// }
+
+// const oldSyntax = new OldSyntax();
+// const getGreeting = oldSyntax.getGreeting;
+// console.log(getGreeting());
+
+
+//--------
+
+// class NewSyntax {
+//     name = 'Tom';
+//     getGreeting = () => {
+//         return `Hi. My name is ${this.name}.`;
+//     }
+// }
+// const newSynstax = new NewSyntax();
+// const newGetGreeting = newSynstax.getGreeting;
+// console.log(newGetGreeting());
